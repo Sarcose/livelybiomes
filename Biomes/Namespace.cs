fe7b9419ab2b92efc0b7862d9471ec255a58b4e0
Sean Critz <sarcose@gmail.com>
2024-04-02 09:52:58 -0500

    GetBiomeValue working

--- Biomes/Namespace.cs
+++ Biomes/Namespace.cs
@@ -1,5 +1,8 @@
 using System;
+using System.Collections.Generic;
+using Genkit;
 using Genkit.SimplexNoise;
+using UnityEngine;
 using XRL;
 using XRL.Core;
 using XRL.Rules;
@@ -7,14 +10,32 @@ using XRL.World;
 using XRL.World.Biomes;
 using XRL.World.Parts.Mutation;
 using XRL.World.ZoneBuilders;
+using XRL.World.ZoneBuilders.Utility;
 
-[HasGameBasedStaticCache]
+using static XRL.World.GameObjectFactory;
+using static XRL.World.GameObjectBlueprint;
+
 namespace Sarcose_Biomes {
     //XRL.Messages.MessageQueue.AddPlayerMessage(message, null, false);
+    [HasModSensitiveStaticCache]
+    public static partial class SarcoseAddBiomes{
+        [ModSensitiveCacheInit]
+        public static void AddNewBiomes()
+        {
+            if (!XRL.World.Biomes.BiomeManager.Biomes.ContainsKey("SaltOasis"))
+            {
+                XRL.World.Biomes.BiomeManager.Biomes.Add("SaltOasis", new SaltOasisBiome());
+            }
+
+        }
+    }
 
     public static partial class Utils{
         public static void sPrintc(string message){
             XRL.Messages.MessageQueue.AddPlayerMessage(message, null, false);
+        }
+        public static void sPrintLog(string message){
+            MetricsManager.LogInfo($"====SB==== {message}");
         }
 
         public class ZoneBound
@@ -28,7 +49,7 @@ namespace Sarcose_Biomes {
             public int? MinZ { get; set; }
 
             // Constructor to initialize the bounds
-            public ZoneBound(int? maxX, int? maxY, int? maxZ, int? minX, int? minY, int? minZ)
+            public ZoneBound(int? maxX = null, int? maxY = null, int? maxZ = null, int? minX = null, int? minY = null, int? minZ = null)
             {
                 MaxX = maxX;
                 MaxY = maxY;
@@ -55,22 +76,14 @@ namespace Sarcose_Biomes {
         }
 
 
-        public static bool CanBuildBiome(string ZoneID, string[] TerrainTypes = {}, int[] StrataRange = {}, ZoneBound bound)
+        public static bool CanBuildBiome(string ZoneID, int num, int num2, int num3, int num4, int num5, string[] TerrainAllowed = null, ZoneBound bound = null)
         {
-            if (!ZoneID.Contains("."))
-			{
-				return false;
-			}
-			string[] array = ZoneID.Split('.');
-			int num = Convert.ToInt32(array[1]);
-			int num2 = Convert.ToInt32(array[2]);
-			int num3 = Convert.ToInt32(array[3]);
-			int num4 = Convert.ToInt32(array[4]);
-			int num5 = Convert.ToInt32(array[5]);
 			if (num5 < 10)
 			{
 				return false;
 			}
+            
+
 			string objectTypeForZone = ZoneManager.GetObjectTypeForZone(num, num2, "JoppaWorld");
 			if (GameObjectFactory.Factory.Blueprints.ContainsKey(objectTypeForZone) && GameObjectFactory.Factory.Blueprints[objectTypeForZone].Tags.ContainsKey("NoBiomes"))
 			{
@@ -80,112 +93,37 @@ namespace Sarcose_Biomes {
 			{
 				return false;
 			}
-            for (int i = 0; i < TerrainTypes.Length; i++)
+            if (TerrainAllowed != null)
             {
-                string Terrain = TerrainTypes[i];
-                //get terrain type and test against terrain type
-            }
-            if ((!StrataRange.Length == 0)){
-                //test if our Strata Range is valid
-            }
-            //TODO: test what var type it is and verify it's compatible with int
-            //TODO: decide whether ParasangX vs ZoneX is worth considering. Psychic uses ParasangX but ZoneZ -- clearly strata is more... stratified than X/Y
-            XRL.World.ZoneID.Parse(ZoneID, out var World, out var ParasangX, out var ParasangY, out var ZoneX, out var ZoneY, out var ZoneZ)
-            if (!bound.IsWithin(ParasangX, ParasangY, ZoneZ)) { return false; }
+                bool allowed = false;
+                Location2D location = new Location2D(num, num2);
+                XRL.World.GameObject terrainObject = ZoneManager.GetTerrainObjectForZone(num, num2, "JoppaWorld");
+                GameObjectBlueprint ZoneBlueprint = XRL.World.GameObjectFactory.Factory.GetBlueprint(terrainObject.Blueprint);
+                string terrain = ZoneBlueprint.GetTag("Terrain");
+                for (int i = 0; i < TerrainAllowed.Length; i++)
+                {
+                    if (TerrainAllowed[i] == terrain){
+                        allowed = true;
+                        break;
+                    }
 
+                }
+                if (!allowed){
+                    return false;
+                    }
+                
+            } else {}
+            if (bound != null)
+            {
+                XRL.World.ZoneID.Parse(ZoneID, out var World, out var ParasangX, out var ParasangY, out var ZoneX, out var ZoneY, out var ZoneZ);
+                if (!bound.IsWithin(ParasangX, ParasangY, ZoneZ)) { 
+                    return false;
+                }
+            }
             return true;
         }
     }
-
-
-    //XRL.World.Biomes.BiomeManagerBiomes.Add("PlayBiome", new PlayBiome());
-
-    public class PlayBiome : IBiome
-    {
-        [NonSerialized]
-        [GameBasedStaticCache(true, false, CreateInstance = false)] //this is verbatim at the top of every biome declaration.
-    	public static byte[,,] BiomeLevels; //this is defined prior to all biomes and seems to be set prior to calling GetBiomeValue()
-
-        public override int GetBiomeValue(string ZoneID)
-        {   //this seems to seed the zone. It seeds the zone to the biome manager, while also seeding the zone for internal logic
-            sPrintc("GetBiomeValue run");
-            //for instance, it sets biomeValue in ZoneID which is used in MutateZone
-            bool cannotBuild = false; //some evaluatio9n
-            //(!ZoneID.Contains(".")) -- if the ZoneID *doesn't* have a . or in other words isn't an address? Maybe?
-            //if it has certain incompatibilities like terrain type or parasang type or strata level?
-            if (cannotBuild){
-                return 0;
-            }
-        }
-
-        public override void MutateZone(Zone Z)
-        {   //this is definitely the bulk of the work here. We have builders being added etc
-            sPrintc("MutateZone run");
-            int biomeValue = GetBiomeValue(Z.ZoneID);
-            //FungalJungle.FungalUpAZone is a function that Fungal uses specifically to populate a zone, probably heavily.
-            if (Z.GetZoneProperty("relaxedbiomes") != "true")
-            {   //variety evaluation
-                if (biomeValue == 1)
-                {
-                    //add zone builders to change the terrain
-                    //new PopTableZoneBuilder().BuildZone(Z, "Slimy1");
-                }
-                if (biomeValue == 2)
-                {
-                    //new PopTableZoneBuilder().BuildZone(Z, "Slimy2");
-                }
-                if (biomeValue == 3)
-                {
-                    //new PopTableZoneBuilder().BuildZone(Z, "Slimy3");
-                }
-            }
-            if (biomeValue >= 1) //some kind of sound evaluation.
-            {
-                //Z.SetZoneProperty("ambient_bed_2", "Sounds/Ambiences/amb_biome_slimy");
-            }
-            //add liquid pools if applicable
-            //new LiquidPools().BuildZone(Z, "SlimePuddle", 3 * GetBiomeValue(Z.ZoneID) - 4, "0", "Slimy Plants");
-
-            //mutate zone inhabitants. For instance, psychic, rusty, etc
-            //see the respective biomes for examples. It spawns GameObjects manually if needed
-
-
-
-
-        }
-
-        public override string MutateZoneName(string Name, string ZoneID, int NameOrder)
-        {
-            sPrintc("MutateZoneName run");
-        }
-
-        public override GameObject MutateGameObject(GameObject Object, string ZoneID)
-        {
-            sPrintc("MutateGameObject run");
-            //use templates to mutate objects.
-        }
-
-        public override bool IsNotable(string ZoneID)
-        {   //probably determines whether or not this shows in the journal - it looks like this is set to false everywhere but rusty
-            //and rusty biomes do seem to be notable...
-            return false;
-        }
-    }
-
 }
-
-
-
-
-/**
-PlayBiome testing:
-
-
-    Different shapes of blocks
-
-**/
-
-
 
 /**
 
--- Biomes/Oasis.cs
+++ Biomes/Oasis.cs
@@ -9,9 +9,10 @@ using XRL.World.Parts.Mutation;
 using XRL.World.ZoneBuilders;
 using static Sarcose_Biomes.Utils;
 
-[HasGameBasedStaticCache]
+
 namespace Sarcose_Biomes {
 
+    [HasGameBasedStaticCache]
     public class SaltOasisBiome : IBiome
     {
         [NonSerialized]
@@ -19,25 +20,84 @@ namespace Sarcose_Biomes {
 
     	public static byte[,,] BiomeLevels;
 
-
         public override int GetBiomeValue(string ZoneID)
         {  
-            bool CanBuild = CanBuildBiome(ZoneID, TerrainTypes: new string[] {"SaltDunes"}, StrataRange: new int[] {0,10}, new ZoneBound(maxX: 100, maxY: 200)); // basic checks for all Biomes 
-            
+            if (!ZoneID.Contains("."))
+			{
+				return 0;
+			}
+            string[] array = ZoneID.Split('.');
+			int num = Convert.ToInt32(array[1]);
+			int num2 = Convert.ToInt32(array[2]);
+			int num3 = Convert.ToInt32(array[3]);
+			int num4 = Convert.ToInt32(array[4]);
+			int num5 = Convert.ToInt32(array[5]);
+            int num6 = 240;
+			int num7 = 75;
+			int num8 = 10;
+			int num9 = num * 3 + num3;
+			int num10 = num2 * 3 + num4;
+			int num11 = num5 % num8;
+
+            bool CanBuild = CanBuildBiome(ZoneID, num, num2, num3, num4, num5, TerrainAllowed: new string[] { "Saltdunes" }, bound: new ZoneBound(maxZ: 10));
+
             //test other biome specific disqualifiers if they apply
             if (!CanBuild){
                 return 0;
             }
-
-            //now build the seed to return
-            int num = 1; //some amount
-            return num; //we can just test if returning any number other than 0 works to generate the biome.
-
+            if (BiomeLevels == null)
+            {
+                BiomeLevels = new byte[num6, num7, num8];
+                LayeredNoise layeredNoise = LayeredNoise.CreateLinearOctiveLayers(3, 1.33f, 0.12f, XRLCore.Core.Game.GetWorldSeed("RustyNoise").ToString());
+                float num12 = 0.66f;
+                float num13 = 0f;
+                float num14 = 1f;
+                float[,,] array2 = layeredNoise.Generate3D(num6, num7, num8);
+                for (int i = 0; i < num8; i++)
+                {
+                    for (int j = 0; j < num7; j++)
+                    {
+                        for (int k = 0; k < num6; k++)
+                        {
+                            float num15 = array2[k, j, i];
+                            num15 = (array2[k, j, i] = ((!(num15 < num12)) ? ((num15 - num12) * (1f / (1f - num12))) : 0f));
+                            if (num15 > num13)
+                            {
+                                num13 = num15;
+                            }
+                            if (num15 < num14)
+                            {
+                                num14 = num15;
+                            }
+                        }
+                    }
+                }
+                for (int l = 0; l < num8; l++)
+                {
+                    for (int m = 0; m < num7; m++)
+                    {
+                        for (int n = 0; n < num6; n++)
+                        {
+                            int num16 = 4;
+                            int num17 = (int)((array2[n, m, l] - num14) / ((num13 - num14) / (float)num16));
+                            if (num17 < 0)
+                            {
+                                num17 = 0;
+                            }
+                            if (num17 > num16 - 1)
+                            {
+                                num17 = num16 - 1;
+                            }
+                            BiomeLevels[n, m, l] = (byte)num17;
+                        }
+                    }
+                }
+            }
+            return BiomeLevels[num9, num10, num11];
         }
 
         public override void MutateZone(Zone Z)
         {   //this is definitely the bulk of the work here. We have builders being added etc
-            sPrintc("MutateZone run");
             int biomeValue = GetBiomeValue(Z.ZoneID);
             //FungalJungle.FungalUpAZone is a function that Fungal uses specifically to populate a zone, probably heavily.
             if (Z.GetZoneProperty("relaxedbiomes") != "true")   //this check seems to be in every one. relaxedbiomes seems to remove builders, perhaps making it less intense
@@ -71,7 +131,7 @@ namespace Sarcose_Biomes {
 
 
             //add liquid pools if applicable
-            //new LiquidPools().BuildZone(Z, "SlimePuddle", 3 * GetBiomeValue(Z.ZoneID) - 4, "0", "Slimy Plants");
+            new LiquidPools().BuildZone(Z, "SlimePuddle", 3 * GetBiomeValue(Z.ZoneID) - 4, "0", "Slimy Plants");
 
             //mutate zone inhabitants. For instance, psychic, rusty, etc
             //see the respective biomes for examples. It spawns GameObjects manually if needed
@@ -83,20 +143,24 @@ namespace Sarcose_Biomes {
 
         public override string MutateZoneName(string Name, string ZoneID, int NameOrder)
         {
-            sPrintc("MutateZoneName run");
+            return IBiome.MutateZoneNameWith(Name, GetBiomeValue(ZoneID), "oasis", "hidden", "secret pool", "sequestered", "oasis", null);
         }
 
         public override GameObject MutateGameObject(GameObject Object, string ZoneID)
         {
-            sPrintc("MutateGameObject run");
+         //   sPrintc("MutateGameObject run");
             //use templates to mutate objects.
+            return Object;  //do not mutate for now
         }
 
-        public override bool IsNotable(string ZoneID)
-        {   //probably determines whether or not this shows in the journal - it looks like this is set to false everywhere but rusty
-            //and rusty biomes do seem to be notable...
-            return false;
+        public override bool IsNotable(string ZoneID)  
+        { 
+            if (GetBiomeValue(ZoneID) > 0){return true;}
+            return false;    
         }
+
+        
+
     }
 
 }
